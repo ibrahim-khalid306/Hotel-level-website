0849deb74ae207df7e61aa09c76e40d6127ba81a
ibrahim-khalid306 <110301331+ibrahim-khalid306@users.noreply.github.com>
2023-01-07 18:18:08 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$( function() {
+    $( "#check-out" ).datepicker();
+  } );
+  $( function() {
+    $( "#check-in" ).datepicker();
+
+    $('.level-slider').slick({
+      dots:true,
+      slidesToShow:2,
+      slidwsToSlides:2,
+      infinite: true,
+      speed: 500,
+      cssEase: 'linear'
+      
+    });
+
+  } );
+
+
+
